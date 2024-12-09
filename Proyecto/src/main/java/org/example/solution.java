30f28afcdd004db89fc7e1d8770d5c7eac9e5aee
A706\1dam <manucarrera34@gmail.com>
2024-12-09 20:14:07 +0100

    Subida

---
+++ Proyecto/src/main/java/org/example/solution.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class solution {
+    static java.util.Scanner in;
+    public static void casoDePrueba() {
+
+        // TU CÓDIGO AQUÍ
+
+    }
+    public static void main(String[] args) {
+
+        in = new java.util.Scanner(System.in);
+
+        int numCasos = in.nextInt();
+        for (int i = 0; i < numCasos; i++)
+            casoDePrueba();
+    }
+}
