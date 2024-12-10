9ab69dc961c3f6f11f2f43a86d96347bdfcd4c59
A706\1dam <manucarrera34@gmail.com>
2024-12-10 19:27:53 +0100

    asdf

--- Proyecto/src/main/java/org/example/Main.java
+++ Proyecto/src/main/java/org/example/Main.java
@@ -3,53 +3,47 @@ package org.example;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * @author: Manuel Carrera
+ * @version: 1.0
+ */
 public class Main {
+    
     static Scanner in;
 
-    /**
-     * @author: Manuel Carrera
-     * @version: 1.0
-     */
     public static void casoDePrueba() {
         Scanner entrada=new Scanner(System.in);
 
         String anyos=entrada.nextLine();
         String[] anyos_texto=anyos.split(" ");
         int[] anos=new int[anyos_texto.length];
-        boolean comp=true;
 
         if (anos.length==4){
             for (int i = 0; i < anos.length; i++) {
-                    try {
-                        anos[i]=Integer.parseInt(anyos_texto[i]);
-                    }catch (NumberFormatException err){
-                        comp=false;
-                        break;
-                    }
+                anos[i]=Integer.parseInt(anyos_texto[i]);
             }
-            if (comp){
-                int anos_ordenado[]=anos.clone();
-                Arrays.sort(anos_ordenado);
 
-                int mayor=anos_ordenado[anos_ordenado.length-1];
-                int menor=anos_ordenado[0];
+            int anos_ordenado[]=anos.clone();
+            Arrays.sort(anos_ordenado);
 
-                if (anos[1]>=anos[2]){
-                    int mayor_pos= Arrays.binarySearch(anos_ordenado, mayor);
-                    int menor_pos= Arrays.binarySearch(anos_ordenado, menor);
+            int mayor=anos_ordenado[anos_ordenado.length-1];
+            int menor=anos_ordenado[0];
 
-                    mayor=anos_ordenado[mayor_pos-1];
-                    menor=anos_ordenado[menor_pos+1];
+            if (anos[1]>=anos[2]){
+                int mayor_pos= Arrays.binarySearch(anos_ordenado, mayor);
+                int menor_pos= Arrays.binarySearch(anos_ordenado, menor);
 
-                    if (mayor-menor<0){
-                        System.out.println(0);
-                    }else {
-                        System.out.println(mayor-menor+1);
-                    }
+                mayor=anos_ordenado[mayor_pos-1];
+                menor=anos_ordenado[menor_pos+1];
 
-                }else {
+                if (mayor-menor<0){
                     System.out.println(0);
+                }else {
+                    System.out.println(mayor-menor+1);
                 }
+
+            }else {
+                System.out.println(0);
             }
         }
     }
